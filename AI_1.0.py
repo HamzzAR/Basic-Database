f0eb9af0a152ce4c7e5df365ef50fd7699ab9af1
HamzzAR <hamza_razeq@hotmail.co.uk>
2016-12-28 23:13:55 +0000

    Update AI_1.0.py
    Added a delete function, which can be used to delete an existing file.

--- AI_1.0.py
+++ AI_1.0.py
@@ -3,11 +3,13 @@ import glob
 import os
 def main():
   print()
-  option2 = int(input('1. Add\n2. Search\n3. Exit\n(1/2/3): '))
+  option2 = int(input('1. Add\n2. Search\n3. Delete\n4. Exit\n(1/2/3): '))
   if option2 == 1:
     add_file()
   elif option2 == 2:
     search_file()
+  elif option2 == 3:
+    delete()
   else:
     print('Exiting...')
     sleep(2)
@@ -21,6 +23,28 @@ def check_dir(p_name):
       reply = False
   return reply
 
+def delete():
+  name = input('Enter name: ')
+  name.lower()
+  option = input('Are you sure you want to DELETE this file?(y/n): ')
+  if option == 'y':
+    print('Deleting..')
+    sleep(2)
+    check = check_dir(name)
+    if check == False:
+      full_name = name + '.txt'
+      os.remove(full_name)
+      print('File Deleted.')
+      sleep(1)
+    else:
+      print('Not Found')
+      sleep(1)
+      main()
+  else:
+    print('Cancelled')
+    sleep(1)
+  main()
+  
 def add_file():
   name = str(input('Enter Name: '))
   name = name.lower()
